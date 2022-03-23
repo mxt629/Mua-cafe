1710e1666c2af6df3d184e03108ab4a89922eb35
mxt629 <xuantien.06021990@gmail.com>
2022-03-23 10:48:46 +0700

    Initial Commit

---
+++ Mua cafe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Mua cafe
+//
+//  Created by Little Chick on 23/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
